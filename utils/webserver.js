c75e09f602cbaac0b8f51f1157277522def76f80
technomechno <davidpro1080@gmail.com>
2024-09-21 11:56:01 -0700

    changed boilerplate

--- utils/webserver.js
+++ utils/webserver.js
@@ -1,56 +1,38 @@
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
-process.env.ASSET_PATH = '/';
+var WebpackDevServer = require("webpack-dev-server"),
+    webpack = require("webpack"),
+    config = require("../webpack.config"),
+    env = require("./env"),
+    path = require("path");
 
-var WebpackDevServer = require('webpack-dev-server'),
-  webpack = require('webpack'),
-  config = require('../webpack.config'),
-  env = require('./env'),
-  path = require('path');
-
-var options = config.chromeExtensionBoilerplate || {};
-var excludeEntriesToHotReload = options.notHotReload || [];
+var options = (config.chromeExtensionBoilerplate || {});
+var excludeEntriesToHotReload = (options.notHotReload || []);
 
 for (var entryName in config.entry) {
   if (excludeEntriesToHotReload.indexOf(entryName) === -1) {
-    config.entry[entryName] = [
-      'webpack/hot/dev-server',
-      `webpack-dev-server/client?hot=true&hostname=localhost&port=${env.PORT}`,
-    ].concat(config.entry[entryName]);
+    config.entry[entryName] =
+      [
+        ("webpack-dev-server/client?http://localhost:" + env.PORT),
+        "webpack/hot/dev-server"
+      ].concat(config.entry[entryName]);
   }
 }
+
+config.plugins =
+  [new webpack.HotModuleReplacementPlugin()].concat(config.plugins || []);
 
 delete config.chromeExtensionBoilerplate;
 
 var compiler = webpack(config);
 
-var server = new WebpackDevServer(
-  {
-    https: false,
+var server =
+  new WebpackDevServer(compiler, {
     hot: true,
-    liveReload: false,
-    client: {
-      webSocketTransport: 'sockjs',
-    },
-    webSocketServer: 'sockjs',
-    host: 'localhost',
-    port: env.PORT,
-    static: {
-      directory: path.join(__dirname, '../build'),
-    },
-    devMiddleware: {
-      publicPath: `http://localhost:${env.PORT}/`,
-      writeToDisk: true,
-    },
+    contentBase: path.join(__dirname, "../build"),
+    sockPort: env.PORT,
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Origin": "*"
     },
-    allowedHosts: 'all',
-  },
-  compiler
-);
+    disableHostCheck: true
+  });
 
-(async () => {
-  await server.start();
-})();
+server.listen(env.PORT);
